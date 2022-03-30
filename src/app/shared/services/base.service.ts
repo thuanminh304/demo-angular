ca7f631a63e946068f0ff6c8918edce5ce1765e2
thuanminh <thuanminh304@gmail.com>
2022-03-30 08:18:02 +0700

    fff

--- src/app/shared/services/base.service.ts
+++ src/app/shared/services/base.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { environment as evn } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { IUserModel } from '../../modules/admin/models/user.model';
 
@@ -7,23 +6,26 @@ import { IUserModel } from '../../modules/admin/models/user.model';
   providedIn: 'root',
 })
 export class BaseService {
+  BASE_URL = 'https://623aba41b5292b8bfcb8db9d.mockapi.io';
+  URL_COMPANY_GROUP = 'https://623acd76f8827fbe47a8bc94.mockapi.io';
+
   constructor(protected _http: HttpClient) {}
 
   get<T>(endpoint: string) {
-    return this._http.get<T>(`${evn.BASE_URL}/${endpoint}`);
+    return this._http.get<T>(`${this.BASE_URL}/${endpoint}`);
   }
   getCG<T>(endpoint: string) {
-    return this._http.get<T>(`${evn.URL_COMPANY_GROUP}/${endpoint}`);
+    return this._http.get<T>(`${this.URL_COMPANY_GROUP}/${endpoint}`);
   }
   post(endpoint: string, formUser: IUserModel) {
-    return this._http.post(`${evn.BASE_URL}/${endpoint}`, formUser);
+    return this._http.post(`${this.BASE_URL}/${endpoint}`, formUser);
   }
 
   put(endpoint: string, formUser: IUserModel) {
-    return this._http.put(`${evn.BASE_URL}/${endpoint}`, formUser);
+    return this._http.put(`${this.BASE_URL}/${endpoint}`, formUser);
   }
 
   delete<T>(endpoint: string) {
-    return this._http.delete<T>(`${evn.BASE_URL}/${endpoint}`);
+    return this._http.delete<T>(`${this.BASE_URL}/${endpoint}`);
   }
 }
